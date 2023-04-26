629a2529554fbfabdcd8de2aeaf356ab33c5b6a6
Joaquin Rus Garate <jrus-gar@c3r5s6.42malaga.com>
2023-04-26 10:55:21 +0200

    v2

--- libreria/ft_memmove.c
+++ libreria/ft_memmove.c
@@ -6,20 +6,54 @@
 /*   By: jrus-gar <jrus-gar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 12:40:35 by jrus-gar          #+#    #+#             */
-/*   Updated: 2023/04/20 15:42:06 by jrus-gar         ###   ########.fr       */
+/*   Updated: 2023/04/26 10:54:47 by jrus-gar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*Copies n characters from src to dest, handling overlappin memory correctly.
+Memmove is a safer approach que memcpy*/
 
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	if (!dest && !src)
-		return (0);
+	size_t				i;
+	unsigned char		*pointer_to_dest;
+	const unsigned char	*pointer_to_src;
+
+	i = 0;
+	pointer_to_dest = dest;
+	pointer_to_src = src;
+	if (dest == src || n == 0)
+		return (dest);
 	if (dest < src)
-		ft_memcpy(dest, src, n);
+	{
+		while (i < n)
+		{
+			*pointer_to_dest = *pointer_to_src;
+			i++;
+			pointer_to_dest++;
+			pointer_to_src++;
+		}
+	}
 	else
-		while (n--)
-			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
+	{
+		while (n > 0)
+		{
+			pointer_to_dest[n - 1] = pointer_to_src[n - 1];
+			n--;
+		}
+	}
 	return (dest);
 }
+
+// int main()
+// {
+// 	char str1[50] = "oldstring";
+// 	char str2[50] = "newstring";
+
+// 	printf("Before memmove str1 = %s, str2 = %s\n", str1, str2);
+// 	ft_memmove(str1, str2, 2);
+// 	printf("After memmove str1 = %s, str2 = %s\n", str1, str2);
+// 	return(0);
+// }
